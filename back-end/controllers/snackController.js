f9e092fe72f3766570da8c90108dd6404287697e
Stephanie <stephaniereyes@pursuit.org>
2022-02-11 18:02:15 -0500

    more routes

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { all } = require("express/lib/application");
 const snacks = express.Router();
-const{getAllSnacks} = require("../queries/snacks");
+const{getAllSnacks,getSnack,createSnack} = require("../queries/snacks");
 
 snacks.get("/",async(req,res)=>{
     try{
@@ -15,6 +15,34 @@ snacks.get("/",async(req,res)=>{
         console.log(err)
     }
 });
+
+snacks.get("/:id",async(req,res)=>{
+    const { id } = req.params;
+    try{
+        const snack = await getSnack(id);
+        if(snack.id){
+        res.status(200).json(snack);
+    } else { 
+        res.status(500).json({error:"Snack not found"});
+    }
+    } catch(err){
+        console.log(err)
+    }
+});
+
+// snacks.post("/",checkName,checkFavorite,async(req,res)=>{
+//     const { body } = req.params;
+//     try{
+//         const createdSnack = await createSnack(body);
+//         if(snack.id){
+//         res.status(200).json(createdSnack);
+//     } else { 
+//         res.status(500).json({error:"Snack not found"});
+//     }
+//     } catch(err){
+//         console.log(err)
+//     }
+// });
 // snacks.get("/", async (req, res) => {
 //     const allSnacks = await getAllSnacks();
 //     if (allSnacks[0]) {
@@ -23,4 +51,5 @@ snacks.get("/",async(req,res)=>{
 //       res.status(500).json({ error: "server error" });
 //     }
 //   });
+ 
 module.exports = snacks;
