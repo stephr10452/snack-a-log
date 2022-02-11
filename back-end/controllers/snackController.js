20536649872daf67fb417c33fca291369caebc14
rob <robwalker@pursuit.org>
2022-02-11 17:52:36 -0500

    snackControl and validations pushing

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const snacks = express.Router();
-const { getAllBookmarks } = require("../queries/snacks");
+const { getAllSnacks } = require("../queries/snacks");
 
 snacks.get("/", async (req, res)=>{
     try{
@@ -15,6 +15,54 @@ snacks.get("/", async (req, res)=>{
     }
 })
 
-module.exports = {
-    snacks,
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+snacks.delete("/;id", async (req, res)=> {
+        const { id } = req.params;
+        const deletedSnack = await deletedSnack(id);
+        if(deletedSnack.id) {
+            res.status(200).json(deletedSnack);
+        } else {
+            res.status(404).json({ error: "Snack not found" });
+        }    
+      })
+
+      snacks.delete("/;id", async (req, res)=> {
+        const { id } = req.params;
+        const { body } = req;
+        const updatedSnack = await updatedSnack(id);
+        if(updatedSnack.id) {
+            res.status(200).json(updatedSnack);
+        } else {
+            res.status(404).json({ error: "Snack not found" });
+        }    
+      })
+
+module.exports = snacks;
