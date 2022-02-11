731b7323c87d32b44367fdba3200161ee5a92442
Alex Tan <alexmyot@yahoo.com>
2022-02-11 18:02:47 -0500

    queries

--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -22,6 +22,46 @@ const getAllSnacks = async() => {
         }
     }
 
+const createSnack = (snack) => {
+    try{
+        const newSnack = await db.one(
+            "INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy) VALUES($1, $2, $3, $4, $5) RETURNING *",
+            [snack.name, snack.fiber, snack.protein, snack.added_sugar, snack.is_healthy]
+        )
+        return newSnack;
+    } catch(error) {
+        return error;
+    }
+    }
 
-module.exports = { getAllSnacks,
-getSnack };
+const deleteSnack = async (id) => {
+    try {
+        const deletedSnack = await db.one(
+            "DELETE FROM snacks WHERE id = $1 RETURNING *",
+            id
+        );
+        return deletedSnack;
+    } catch (error) {
+        return error;
+    }
+}
+
+const updateSnack = async (id, snack) => {
+    try {
+        const updatedSnack = await db.one(
+            "UPDATE snacks SET name=$1, fiber=$2, protein=$3, added_sugar=$4, is_healthy=$5, WHERE id=$6 RETURNING *",
+            [snack.name, snack.fiber, snack.protein, snack.added_sugar, snack.is_healthy, id]
+        );
+        return updatedSnack;
+    } catch (error) {
+        return error;
+    }
+}
+
+module.exports = {
+getAllSnacks,
+getSnack,
+createSnack,
+deleteSnack,
+updateSnack,
+};
