1c89de6995caf3d26ce29b8ba261f6604abf486f
Alex Tan <alexmyot@yahoo.com>
2022-02-13 10:19:39 -0500

    Pages

---
+++ front-end/src/Pages/Edit.js
@@ -0,0 +1,12 @@
+import SnackEdit from "../Components/SnackEdit";
+
+function Edit() {
+  return (
+    <div className="New Edit">
+      <h2>Edit</h2>
+      <SnackEdit />
+    </div>
+  );
+}
+
+export default Edit;
