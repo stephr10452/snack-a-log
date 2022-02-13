5a8869a26f58f7051e5206f47ab03013c7fd136d
rob <robwalker@pursuit.org>
2022-02-13 03:57:54 -0500

    Added edit, new and snacks in components

---
+++ front-end/src/Components/Snacks.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { useState, useEffect} from 'react';
+import Snack from './Snack';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+function Snacks() {
+    const [snacks, setSnacks] = useState ([]);
+
+    useEffect(() => {
+        axios
+          .get(`${API_URL}/snacks`)
+          .then((res) => {
+              setSnacks(res.data);
+        }).catch((error) => {
+              throw error
+      })
+    }, []);
+
+    return (
+        <div>
+            <div>
+                <section>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Image</th>
+                                <th>Fiber</th>
+                                <th>Protein</th>
+                                <th>Added Sugar</th>
+                                <th>Is Healthy</th> 
+                            </tr>
+                        </thead>
+                         <tbody>
+                             {snacks.map((snack, id) => {
+                                 return ( <Snack key={id} snack={snack} index={id} /> );
+                             })}
+                         </tbody>
+                    </table>
+                </section>
+            </div>
+        </div>
+    );
+}
+
+export default Snacks;
