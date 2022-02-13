2ae11b5741eaaaf94e33b136e392bc2ecb19f077
Stephanie <stephaniereyes@pursuit.org>
2022-02-13 12:12:36 -0500

    changed snacks

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -7,9 +7,12 @@ snacks.get("/", async (req, res)=>{
     try{
         const allSnacks = await getAllSnacks();
         if(allSnacks[0]){
-            res.status(200).json(allSnacks);
+            res.status(200).json({
+                success:true,
+                payload: allSnacks
+            });
         } else {
-            res.status(500).json({ error: "server error" });
+            res.status(404).json({ error: "server error" });
         }    
       } catch(err){
         console.log(err);
