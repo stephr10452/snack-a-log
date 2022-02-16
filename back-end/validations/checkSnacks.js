c9ef13c4d20f1032138f202a42887f91020a3352
rob <robwalker@pursuit.org>
2022-02-15 23:51:08 -0500

    fix typo

--- back-end/validations/checkSnacks.js
+++ back-end/validations/checkSnacks.js
@@ -2,7 +2,7 @@ const checkName = (req, res, next) => {
     let nameStr = req.body.name;
     let nameStrSpl = nameStr.split(' ').map((word) => {
         if(word.length > 2) {
-            word = word.chatAt(0).toUpperCase() + word.slice(1).toLowerCase()
+            word = word.chartAt(0).toUpperCase() + word.slice(1).toLowerCase()
         } else if (word.length < 3) {
             word = word.toLowerCase(0);
         }
